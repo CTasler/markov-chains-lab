647e4ba657fb1628a4d3b4290ddf7093fc425965
Claire Tasler <tasl0368@gmail.com>
2022-10-04 18:50:37 -0500

    removed comments and fixed organization, added sys.argv

--- markov.py
+++ markov.py
@@ -10,12 +10,8 @@ def open_and_read_file(file_path):
     Takes a string that is a file path, opens the file, and turns
     the file's contents as one string of text.
     """
-
-    # your code goes here
     content = open(file_path).read()
-
     print(content)
-
     return content
 
 
@@ -48,24 +44,12 @@ def make_chains(content):
     words = content.split()
     value = []
     for i in range(len(words) - 2):
-        #print(words[i], words[i + 1], words[i + 2])
-        # chains[(words[i], words[i + 1])] = chains.get(None, 0)
-        # if (words[i], words[i + 1]) in chains: 
-        #     value.append(words[i + 2])
-        #     chains[(words[i], words[i +1])] = value
-        #     value = []
-        #print(chains)
         key = (words[i], words[i + 1])
         value = words[i + 2]
-        #print(key)
-        #print(value)
         if key not in chains:
             chains[key] = []
         chains[key].append(value)
     print(chains)
-        
-        
-
     return chains
 
 
@@ -76,49 +60,25 @@ def make_text(chains):
     all_keys = []
   
     for key in chains.keys():
-        #print(key)
         all_keys.append(key)
        
     random_key = choice(all_keys)
-    #print(random_key)
     final_sentence = " ".join(list(random_key))
-    
-
-    ######################
-
     new_value = choice(chains[random_key])
     final_sentence += f" {new_value} "
-   
     new_key = (random_key[1], new_value)
-    #print(new_key)
-    # for item in random_key:
-    #     final_sentence += f"{item} "
-    #     random_value = choice(chains[random_key])
-    #     final_sentence += f"{random_value} "
 
     while new_key in chains: 
         new_value = choice(chains[new_key])
         final_sentence += f"{new_value} "
         new_key = (new_key[1], new_value)
-        #print(final_sentence)
    
-    print(final_sentence)
-    # print(random_value)
-    
-    return ' '.join(words)
+    print(final_sentence.capitalize())
+    return final_sentence.capitalize()
 
 
-input_path = 'gettysburg.txt'
-
-# Open the file and turn it into one long string
+input_path = sys.argv[1]
 input_text = open_and_read_file(input_path)
-
-# Get a Markov chain
 chains = make_chains(input_text)
-
-# Produce random text
 random_text = make_text(chains)
-
 print(random_text)
-
-# fernpereg97 fernandaperezgutierrez1@gmail.com
